6d0ac4d6baf15811afd1ca845070a1c79c3a0548
Razvan Nicu <razvan.nicu@wealth-dynamix.com>
2025-04-16 18:40:33 +0100

    add tooltips

--- assets/js/field.js
+++ assets/js/field.js
@@ -7,6 +7,28 @@ import tippy from 'tippy.js';
 import storageManager from './modules/storageManager.js';
 import sort from '@alpinejs/sort';
 
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Alpine.js is initializing...');
+  Alpine.data('fieldComp', fieldComp);
+  Alpine.data('storageManager', storageManager);
+
+  // Start Alpine
+  Alpine.plugin(sort);
+  Alpine.start();
+});
+
+Alpine.magic('tooltip', (el) => (message, duration = 1000) => {
+  let instance = tippy(el, { content: message, trigger: 'manual' });
+
+  instance.show();
+
+  setTimeout(() => {
+    instance.hide();
+
+    setTimeout(() => instance.destroy(), 50);
+  }, duration);
+});
+
 document.addEventListener('alpine:init', () => {
   // Magic: $tooltip
   Alpine.magic('tooltip', (el) => (message) => {
@@ -17,7 +39,7 @@ document.addEventListener('alpine:init', () => {
     setTimeout(() => {
       instance.hide();
 
-      setTimeout(() => instance.destroy(), 150);
+      setTimeout(() => instance.destroy(), 50);
     }, 2000);
   });
 
@@ -25,14 +47,4 @@ document.addEventListener('alpine:init', () => {
   Alpine.directive('tooltip', (el, { expression }) => {
     tippy(el, { content: expression });
   });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Alpine.js is initializing...');
-  Alpine.data('fieldComp', fieldComp);
-  Alpine.data('storageManager', storageManager);
-
-  // Start Alpine
-  Alpine.plugin(sort);
-  Alpine.start();
 });
